b56d5bec0c898055f15060639b1b400d67c65b20
Muskan Sondhiya <muskansondhiya380@gmail.com>
2026-07-27 16:46:11 +0530

    Optimize memory usage for Render deployment

--- processor_bert.py
+++ processor_bert.py
@@ -1,29 +1,41 @@
 import joblib
 from sentence_transformers import SentenceTransformer
 
-model_embedding = SentenceTransformer('all-MiniLM-L6-v2')  # Lightweight embedding model
-model_classification = joblib.load("models/log_classifier.joblib")
+model_embedding = None
+model_classification = None
+
+
+def load_models():
+    global model_embedding
+    global model_classification
+
+    if model_embedding is None:
+        model_embedding = SentenceTransformer("all-MiniLM-L6-v2")
+
+    if model_classification is None:
+        model_classification = joblib.load("models/log_classifier.joblib")
 
 
 def classify_with_bert(log_message):
-    embeddings = model_embedding.encode([log_message])
-    probabilities = model_classification.predict_proba(embeddings)[0]
+
+    load_models()
+
+    embedding = model_embedding.encode([log_message])
+
+    probabilities = model_classification.predict_proba(embedding)[0]
+
     if max(probabilities) < 0.5:
         return "Unclassified"
-    predicted_label = model_classification.predict(embeddings)[0]
-    
-    return predicted_label
+
+    return model_classification.predict(embedding)[0]
 
 
 if __name__ == "__main__":
     logs = [
-        "alpha.osapi_compute.wsgi.server - 12.10.11.1 - API returned 404 not found error",
-        "GET /v2/3454/servers/detail HTTP/1.1 RCODE   404 len: 1583 time: 0.1878400",
-        "System crashed due to drivers errors when restarting the server",
-        "Hey bro, chill ya!",
-        "Multiple login failures occurred on user 6454 account",
-        "Server A790 was restarted unexpectedly during the process of data transfer"
+        "alpha.osapi_compute.wsgi.server - API returned 404",
+        "Server crashed",
+        "User login failed"
     ]
+
     for log in logs:
-        label = classify_with_bert(log)
-        print(log, "->", label)
+        print(classify_with_bert(log))
